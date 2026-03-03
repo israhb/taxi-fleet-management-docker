0948ad78af13a21cbb00a528cbf6a336b7bb8b6f
Heber Israel <heber_ji@hotmail.com>
2026-03-03 17:54:45 -0600

    se agregan cambios con configuracion inicial para base de datos h2

---
+++ src/main/java/com/mentedigital/israel/taxis/TaxisApplication.java
@@ -0,0 +1,13 @@
+package com.mentedigital.israel.taxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentedigital/israel/taxis/TaxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentedigital.israel.taxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
